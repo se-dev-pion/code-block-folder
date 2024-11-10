3d777ed903a8e98bd18b06630ee9a206d279e732
BioCrossCoder <viscaria_dignics@163.com>
2024-11-10 14:24:21 +0800

    [feat] Add highlight to comments that with folding

--- src/extension.ts
+++ src/extension.ts
@@ -1,4 +1,3 @@
-
 import * as vscode from 'vscode';
 import { loadFolder } from './folder';
 import { highlightTitle } from './highlight';
--- src/highlight.ts
+++ src/highlight.ts
@@ -1,5 +1,35 @@
 import vscode from 'vscode';
+import { isSingleLineCommentWithPrefix } from './common';
 
 export function highlightTitle(context: vscode.ExtensionContext) {
-
+    const decorationType = vscode.window.createTextEditorDecorationType({
+        backgroundColor: '#003366',
+        color: '#FFFFFF',
+        opacity: '0.8'
+    });
+    const updateDecorations = () => {
+        const editor = vscode.window.activeTextEditor;
+        if (!editor) {
+            return;
+        }
+        const document: vscode.TextDocument = editor.document;
+        const decorations = new Array<vscode.DecorationOptions>();
+        for (let i = 0; i < document.lineCount; i++) {
+            const line = document.lineAt(i);
+            if (isSingleLineCommentWithPrefix(line.text, document.languageId, '[')) {
+                const left = line.text.indexOf("[");
+                const start = line.range.start.translate(0, left);
+                const right = line.text.indexOf("]");
+                const end = (right !== -1) ? line.range.start.translate(0, right + 1) : line.range.end;
+                const range = line.range.with(start, end);
+                const decoration = { range: range };
+                decorations.push(decoration);
+            }
+        }
+        editor.setDecorations(decorationType, decorations);
+    };
+    updateDecorations();
+    vscode.workspace.onDidChangeTextDocument(updateDecorations);
+    vscode.window.onDidChangeActiveTextEditor(updateDecorations);
+    vscode.window.onDidChangeTextEditorVisibleRanges(updateDecorations);
 }
