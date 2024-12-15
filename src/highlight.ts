d560f3596c53ae5890815392858a12efb784a9ab
BioCrossCoder <viscaria_dignics@163.com>
2024-12-15 16:09:28 +0800

    [feature] add decoration to end tags

--- src/highlight.ts
+++ src/highlight.ts
@@ -1,28 +1,43 @@
 import vscode from 'vscode';
-import { debounced, registerFoldableBlocks, titlePrefix, titleSuffix } from './common';
+import { debounced, endTag, registerFoldableBlocks, titlePrefix, titleSuffix } from './common';
 
 export function highlightTitle(_context: vscode.ExtensionContext) {
-    // [DefiniteHighlightStyle]
-    const decorationType = vscode.window.createTextEditorDecorationType({
+    // [DefiniteHighlightStyles]
+    const titleDecoration = vscode.window.createTextEditorDecorationType({
         backgroundColor: new vscode.ThemeColor('editor.foreground'),
         color: new vscode.ThemeColor('editor.background'),
         fontWeight: 'bold',
     });
-    // [/]
+    const endingDecoration = vscode.window.createTextEditorDecorationType({
+        borderColor: new vscode.ThemeColor('editor.foreground'),
+        borderWidth: '2px',
+        borderStyle: 'solid',
+    }); // [/]
     const updateDecorations = debounced(async () => {
-        // [AddHighlightToTitles]
         for (const editor of vscode.window.visibleTextEditors) {
             const document: vscode.TextDocument = editor.document;
-            const handler = (document: vscode.TextDocument, stack: number[], _end: number) => {
+            // [AddHighlightToTitles]
+            const handleTitle = (document: vscode.TextDocument, stack: number[], _end: number) => {
                 const line = document.lineAt(stack.pop() as number);
-                const left = line.text.indexOf(titlePrefix);
-                const start = line.range.start.translate(0, left);
-                const right = line.text.indexOf(titleSuffix);
-                const end = (right !== -1) ? line.range.start.translate(0, right + 1) : line.range.end;
-                return { range: line.range.with(start, end) };
+                const leftBorder = line.text.indexOf(titlePrefix);
+                const rangeStart = line.range.start.translate(0, leftBorder);
+                const rightBorder = line.text.indexOf(titleSuffix);
+                const rangeEnd = (rightBorder !== -1) ? line.range.start.translate(0, rightBorder + 1) : line.range.end;
+                return { range: line.range.with(rangeStart, rangeEnd) };
             };
-            editor.setDecorations(decorationType, registerFoldableBlocks(document, handler));
-        } // [/]
+            editor.setDecorations(titleDecoration, registerFoldableBlocks(document, handleTitle)); // [/]
+            // [AddHighlightToEndings]
+            const handleEnding = (document: vscode.TextDocument, _stack: number[], end: number) => {
+                const line = document.lineAt(end);
+                const leftBorder = line.text.indexOf(endTag);
+                const rangeStart = line.range.start.translate(0, leftBorder);
+                const rightBorder = leftBorder + endTag.length;
+                const rangeEnd = line.range.start.translate(0, rightBorder + 1);
+                return { range: line.range.with(rangeStart, rangeEnd) };
+            };
+            const endings = registerFoldableBlocks(document, handleEnding);
+            editor.setDecorations(endingDecoration, endings); // [/]
+        }
     }, 50);
     updateDecorations();
     // [AddEventListeners]
